bd4dbad1b66b1d4a309de655b69a846b9a948a66
PrinceGosplana <nevkluchat@gmail.com>
2012-10-20 17:08:37 +0300

    Add validation

---
+++ StoreCard/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StoreCard.Models
+{
+    [MetadataType(typeof(CardMetadata))]
+    public partial class Card
+    {
+
+    }
+    public class CardMetadata
+    {
+        [Required(ErrorMessage = "Amount is required")]
+        [DisplayName("Amount in the card")]
+        public decimal Amount { get; set; }
+    }
+}
